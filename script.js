4edcc59e4b451eafc85885dacab0757b0f86a94c
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2025-01-06 20:47:26 +0000

    remove console.logs do script

--- script.js
+++ script.js
@@ -1,206 +1,149 @@
-
 function calcular() {
-  let pontosSobra = document.getElementById('pontosTotal').value;
+  let pontosSobra = document.getElementById("pontosTotal").value;
 
   //-força
 
-  var valorForca = document.getElementById('forcaValor')
-  var racaForca = document.getElementById('forcaRaca')
-  var bonusForca = document.getElementById('forcaBonus')
+  var valorForca = document.getElementById("forcaValor");
+  var racaForca = document.getElementById("forcaRaca");
+  var bonusForca = document.getElementById("forcaBonus");
 
-  var valorInicFor = Number(valorForca.value)
-  var valorRacaFor = Number(racaForca.value)
-  var valorBonusFor = Number(bonusForca.value)
+  var valorInicFor = Number(valorForca.value);
+  var valorRacaFor = Number(racaForca.value);
+  var valorBonusFor = Number(bonusForca.value);
 
-  var totalForca = ''
-  if
-    (valorInicFor > '3') {
-    valorInicFor = 4
-    document.querySelector('input[id="forcaValor"]').value = '4'
-    console.log("oi")
-
-  } else {
-    console.log("11111")
+  var totalForca = "";
+  if (valorInicFor > "3") {
+    valorInicFor = 4;
+    document.querySelector('input[id="forcaValor"]').value = "4";
   }
 
-  if (valorInicFor < '-1') {
-    valorInicFor = -1
-    document.querySelector('input[id="forcaValor"]').value = '-1'
-
-  } else {
-    console.log("rrrrrr")
+  if (valorInicFor < "-1") {
+    valorInicFor = -1;
+    document.querySelector('input[id="forcaValor"]').value = "-1";
   }
 
-  totalForca = (valorInicFor + valorRacaFor + valorBonusFor)
-  document.querySelector('input[id="forcaTotal"]').value = ` ${totalForca}`
+  totalForca = valorInicFor + valorRacaFor + valorBonusFor;
+  document.querySelector('input[id="forcaTotal"]').value = ` ${totalForca}`;
 
+  /////////////////////////////dest//////////////////////////////////////////////
+  var valorDest = document.getElementById("destValor");
+  var racaDest = document.getElementById("destRaca");
+  var bonusDest = document.getElementById("destBonus");
 
+  var valorInicDest = Number(valorDest.value);
+  var valorRacaDest = Number(racaDest.value);
+  var valorBonusDest = Number(bonusDest.value);
 
-
-  /////////////////////////////dest//////////////////////////////////////////////    
-  var valorDest = document.getElementById('destValor')
-  var racaDest = document.getElementById('destRaca')
-  var bonusDest = document.getElementById('destBonus')
-
-  var valorInicDest = Number(valorDest.value)
-  var valorRacaDest = Number(racaDest.value)
-  var valorBonusDest = Number(bonusDest.value)
-
-  var totalDest = ''
-  if
-    (valorInicDest > '3') {
-    valorInicDest = 4
-    document.querySelector('input[id="destValor"]').value = '4'
-    console.log("oi")
-
-  } else {
-    console.log("11111")
+  var totalDest = "";
+  if (valorInicDest > "3") {
+    valorInicDest = 4;
+    document.querySelector('input[id="destValor"]').value = "4";
   }
 
-  if (valorInicDest < '-1') {
-    valorInicDest = -1
-    document.querySelector('input[id="destValor"]').value = '-1'
-    console.log("oi6")
-  } else {
-    console.log("rrrrrr")
+  if (valorInicDest < "-1") {
+    valorInicDest = -1;
+    document.querySelector('input[id="destValor"]').value = "-1";
   }
 
+  totalDest = valorInicDest + valorRacaDest + valorBonusDest;
+  document.querySelector('input[id="destTotal"]').value = ` ${totalDest}`;
 
-  totalDest = (valorInicDest + valorRacaDest + valorBonusDest)
-  document.querySelector('input[id="destTotal"]').value = ` ${totalDest}`
+  //////////////////////////const/////////////////////////////////////////////////
+  var valorConst = document.getElementById("constValor");
+  var racaConst = document.getElementById("constRaca");
+  var bonusConst = document.getElementById("constBonus");
 
-  //////////////////////////const/////////////////////////////////////////////////    
-  var valorConst = document.getElementById('constValor')
-  var racaConst = document.getElementById('constRaca')
-  var bonusConst = document.getElementById('constBonus')
+  var valorInicConst = Number(valorConst.value);
+  var valorRacaConst = Number(racaConst.value);
+  var valorBonusConst = Number(bonusConst.value);
 
-  var valorInicConst = Number(valorConst.value)
-  var valorRacaConst = Number(racaConst.value)
-  var valorBonusConst = Number(bonusConst.value)
-
-  var totalConst = ''
-  if
-    (valorInicConst > '3') {
-    valorInicConst = 4
-    document.querySelector('input[id="constValor"]').value = '4'
-    console.log("oi")
-
-  } else {
-    console.log("11111")
+  var totalConst = "";
+  if (valorInicConst > "3") {
+    valorInicConst = 4;
+    document.querySelector('input[id="constValor"]').value = "4";
   }
 
-  if (valorInicConst < '-1') {
-    valorInicConst = -1
-    document.querySelector('input[id="constValor"]').value = '-1'
-    console.log("oi6")
-  } else {
-    console.log("rrrrrr")
+  if (valorInicConst < "-1") {
+    valorInicConst = -1;
+    document.querySelector('input[id="constValor"]').value = "-1";
   }
 
+  totalConst = valorInicConst + valorRacaConst + valorBonusConst;
+  document.querySelector('input[id="constTotal"]').value = ` ${totalConst}`;
 
-  totalConst = (valorInicConst + valorRacaConst + valorBonusConst)
-  document.querySelector('input[id="constTotal"]').value = ` ${totalConst}`
+  ///////////////////////////inte////////////////////////////////////////////////
+  var valorInt = document.getElementById("intValor");
+  var racaInt = document.getElementById("intRaca");
+  var bonusInt = document.getElementById("intBonus");
 
-  ///////////////////////////inte////////////////////////////////////////////////    
-  var valorInt = document.getElementById('intValor')
-  var racaInt = document.getElementById('intRaca')
-  var bonusInt = document.getElementById('intBonus')
+  var valorInicInt = Number(valorInt.value);
+  var valorRacaInt = Number(racaInt.value);
+  var valorBonusInt = Number(bonusInt.value);
 
-  var valorInicInt = Number(valorInt.value)
-  var valorRacaInt = Number(racaInt.value)
-  var valorBonusInt = Number(bonusInt.value)
-
-  var totalInt = ''
-  if
-    (valorInicInt > '3') {
-    valorInicInt = 4
-    document.querySelector('input[id="intValor"]').value = '4'
-    console.log("oi")
-
-  } else {
-    console.log("11111")
+  var totalInt = "";
+  if (valorInicInt > "3") {
+    valorInicInt = 4;
+    document.querySelector('input[id="intValor"]').value = "4";
   }
 
-  if (valorInicInt < '-1') {
-    valorInicInt = -1
-    document.querySelector('input[id="intValor"]').value = '-1'
-    console.log("oi6")
-  } else {
-    console.log("rrrrrr")
+  if (valorInicInt < "-1") {
+    valorInicInt = -1;
+    document.querySelector('input[id="intValor"]').value = "-1";
   }
 
+  totalInt = valorInicInt + valorRacaInt + valorBonusInt;
+  document.querySelector('input[id="intTotal"]').value = ` ${totalInt}`;
 
-  totalInt = (valorInicInt + valorRacaInt + valorBonusInt)
-  document.querySelector('input[id="intTotal"]').value = ` ${totalInt}`
+  ////////////////////////////sabed///////////////////////////////////////////////
+  var valorSab = document.getElementById("sabValor");
+  var racaSab = document.getElementById("sabRaca");
+  var bonusSab = document.getElementById("sabBonus");
 
-  ////////////////////////////sabed///////////////////////////////////////////////    
-  var valorSab = document.getElementById('sabValor')
-  var racaSab = document.getElementById('sabRaca')
-  var bonusSab = document.getElementById('sabBonus')
+  var valorInicSab = Number(valorSab.value);
+  var valorRacaSab = Number(racaSab.value);
+  var valorBonusSab = Number(bonusSab.value);
 
-  var valorInicSab = Number(valorSab.value)
-  var valorRacaSab = Number(racaSab.value)
-  var valorBonusSab = Number(bonusSab.value)
-
-  var totalSab = ''
-  if
-    (valorInicSab > '3') {
-    valorInicSab = 4
-    document.querySelector('input[id="sabValor"]').value = '4'
-    console.log("oi")
-
-  } else {
-    console.log("11111")
+  var totalSab = "";
+  if (valorInicSab > "3") {
+    valorInicSab = 4;
+    document.querySelector('input[id="sabValor"]').value = "4";
   }
 
-  if (valorInicSab < '-1') {
-    valorInicSab = -1
-    document.querySelector('input[id="sabValor"]').value = '-1'
-    console.log("oi6")
-  } else {
-    console.log("rrrrrr")
+  if (valorInicSab < "-1") {
+    valorInicSab = -1;
+    document.querySelector('input[id="sabValor"]').value = "-1";
   }
 
-
-  totalSab = (valorInicSab + valorRacaSab + valorBonusSab)
-  document.querySelector('input[id="sabTotal"]').value = ` ${totalSab}`
+  totalSab = valorInicSab + valorRacaSab + valorBonusSab;
+  document.querySelector('input[id="sabTotal"]').value = ` ${totalSab}`;
 
   ////////////////////////////caris///////////////////////////////////////////////
 
-  var valorCar = document.getElementById('carValor')
-  var racaCar = document.getElementById('carRaca')
-  var bonusCar = document.getElementById('carBonus')
+  var valorCar = document.getElementById("carValor");
+  var racaCar = document.getElementById("carRaca");
+  var bonusCar = document.getElementById("carBonus");
 
-  var valorInicCar = Number(valorCar.value)
-  var valorRacaCar = Number(racaCar.value)
-  var valorBonusCar = Number(bonusCar.value)
+  var valorInicCar = Number(valorCar.value);
+  var valorRacaCar = Number(racaCar.value);
+  var valorBonusCar = Number(bonusCar.value);
 
-  var totalCar = ''
-  if
-    (valorInicCar > '3') {
-    valorInicCar = 4
-    document.querySelector('input[id="carValor"]').value = '4'
-    console.log("oi")
-
-  } else {
-    console.log("11111")
+  var totalCar = "";
+  if (valorInicCar > "3") {
+    valorInicCar = 4;
+    document.querySelector('input[id="carValor"]').value = "4";
   }
 
-  if (valorInicCar < '-1') {
-    valorInicCar = -1
-    document.querySelector('input[id="carValor"]').value = '-1'
-    console.log("oi6")
-  } else {
-    console.log("rrrrrr")
+  if (valorInicCar < "-1") {
+    valorInicCar = -1;
+    document.querySelector('input[id="carValor"]').value = "-1";
   }
 
-
-  totalCar = (valorInicCar + valorRacaCar + valorBonusCar)
-  document.querySelector('input[id="carTotal"]').value = ` ${totalCar}`
+  totalCar = valorInicCar + valorRacaCar + valorBonusCar;
+  document.querySelector('input[id="carTotal"]').value = ` ${totalCar}`;
 
   ////conversor de valor em pontos Força///
   let forca = 0;
-  const valorFlutFor = parseFloat(document.getElementById('forcaValor').value);
+  const valorFlutFor = parseFloat(document.getElementById("forcaValor").value);
 
   switch (valorFlutFor) {
     case -1:
@@ -221,17 +164,15 @@ function calcular() {
     case 4:
       forca = 7;
       break;
-
   }
-  console.log(`aqui é ${forca}`);
 
-  let custoForca = document.getElementById("forcaCusto").value
-  custoForca = (forca)
-  document.querySelector('input[id="forcaCusto"]').value = `${custoForca}`
+  let custoForca = document.getElementById("forcaCusto").value;
+  custoForca = forca;
+  document.querySelector('input[id="forcaCusto"]').value = `${custoForca}`;
 
   ////////////////Destreza
   let destreza = 0;
-  const valorFlutDest = parseFloat(document.getElementById('destValor').value);
+  const valorFlutDest = parseFloat(document.getElementById("destValor").value);
 
   switch (valorFlutDest) {
     case -1:
@@ -252,17 +193,17 @@ function calcular() {
     case 4:
       destreza = 7;
       break;
-
   }
-  console.log(`aqui é ${destreza}`);
 
-  let custoDestreza = document.getElementById("destCusto").value
-  custoDestreza = (destreza)
-  document.querySelector('input[id="destCusto"]').value = `${custoDestreza}`
+  let custoDestreza = document.getElementById("destCusto").value;
+  custoDestreza = destreza;
+  document.querySelector('input[id="destCusto"]').value = `${custoDestreza}`;
 
-  ////////////////Constituição 
+  ////////////////Constituição
   let constituicao = 0;
-  const valorFlutConst = parseFloat(document.getElementById('constValor').value);
+  const valorFlutConst = parseFloat(
+    document.getElementById("constValor").value
+  );
 
   switch (valorFlutConst) {
     case -1:
@@ -283,17 +224,17 @@ function calcular() {
     case 4:
       constituicao = 7;
       break;
-
   }
-  console.log(`aqui é ${constituicao}`);
 
-  let custoConstituicao = document.getElementById("constCusto").value
-  custoConstituicao = (constituicao)
-  document.querySelector('input[id="constCusto"]').value = `${custoConstituicao}`
+  let custoConstituicao = document.getElementById("constCusto").value;
+  custoConstituicao = constituicao;
+  document.querySelector(
+    'input[id="constCusto"]'
+  ).value = `${custoConstituicao}`;
 
-  ////////////////Inteligência 
+  ////////////////Inteligência
   let inteligencia = 0;
-  const valorFlutInt = parseFloat(document.getElementById('intValor').value);
+  const valorFlutInt = parseFloat(document.getElementById("intValor").value);
 
   switch (valorFlutInt) {
     case -1:
@@ -314,17 +255,15 @@ function calcular() {
     case 4:
       inteligencia = 7;
       break;
-
   }
-  console.log(`aqui é ${inteligencia}`);
 
-  let custoInteligencia = document.getElementById("intCusto").value
-  custoInteligencia = (inteligencia)
-  document.querySelector('input[id="intCusto"]').value = `${custoInteligencia}`
+  let custoInteligencia = document.getElementById("intCusto").value;
+  custoInteligencia = inteligencia;
+  document.querySelector('input[id="intCusto"]').value = `${custoInteligencia}`;
 
-  ////////////////Sabedoria 
+  ////////////////Sabedoria
   let sabedoria = 0;
-  const valorFlutSab = parseFloat(document.getElementById('sabValor').value);
+  const valorFlutSab = parseFloat(document.getElementById("sabValor").value);
 
   switch (valorFlutSab) {
     case -1:
@@ -345,17 +284,15 @@ function calcular() {
     case 4:
       sabedoria = 7;
       break;
-
   }
-  console.log(`aqui é ${sabedoria}`);
 
-  let custoSabedoria = document.getElementById("sabCusto").value
-  custoSabedoria = (sabedoria)
-  document.querySelector('input[id="sabCusto"]').value = `${custoSabedoria}`
+  let custoSabedoria = document.getElementById("sabCusto").value;
+  custoSabedoria = sabedoria;
+  document.querySelector('input[id="sabCusto"]').value = `${custoSabedoria}`;
 
-  ////////////////Carisma 
+  ////////////////Carisma
   let carisma = 0;
-  const valorFlutCar = parseFloat(document.getElementById('carValor').value);
+  const valorFlutCar = parseFloat(document.getElementById("carValor").value);
 
   switch (valorFlutCar) {
     case -1:
@@ -376,29 +313,31 @@ function calcular() {
     case 4:
       carisma = 7;
       break;
-
   }
-  console.log(`aqui é ${carisma}`);
 
-  let custoCarisma = document.getElementById("carCusto").value
-  custoCarisma = (carisma)
-  document.querySelector('input[id="carCusto"]').value = `${custoCarisma}`
-
+  let custoCarisma = document.getElementById("carCusto").value;
+  custoCarisma = carisma;
+  document.querySelector('input[id="carCusto"]').value = `${custoCarisma}`;
 
   ///////cálculo para subtração dos pontos///////
 
-  let sobraPontos = 10
+  let sobraPontos = 10;
 
-  sobraPontos -= custoForca + custoDestreza + custoConstituicao + custoInteligencia + custoSabedoria + custoCarisma
+  sobraPontos -=
+    custoForca +
+    custoDestreza +
+    custoConstituicao +
+    custoInteligencia +
+    custoSabedoria +
+    custoCarisma;
 
-  document.querySelector('input[id="pontosTotal"]').value = `${sobraPontos}`
+  document.querySelector('input[id="pontosTotal"]').value = `${sobraPontos}`;
 
   if (sobraPontos < 0) {
-    document.getElementById("pontosTotal").style.color = "red"
+    document.getElementById("pontosTotal").style.color = "red";
   } else {
-    document.getElementById("pontosTotal").style.color = "#1a480aff"
+    document.getElementById("pontosTotal").style.color = "#1a480aff";
   }
-
 }
 
 //var resposta = document.getElementById('forcaTotal')//
